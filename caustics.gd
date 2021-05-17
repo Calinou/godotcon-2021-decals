381450435f22c4a54b0a496ec66e605024bbb7ee
Hugo Locurcio <hugo.locurcio@hugo.pro>
2021-05-17 19:34:17 +0200

    Add cloud shadows scene

--- caustics.gd
+++ caustics.gd
@@ -3,8 +3,8 @@ extends Decal
 var caustic_id := 0
 var timer := 0.0
 
-func _physics_process(delta):
-	timer += delta
+func _physics_process(some_timer):
+	timer += some_timer
 	if timer >= 0.04:  # 25 FPS
 		timer = 0.0
 		caustic_id = (caustic_id + 1) % 32
