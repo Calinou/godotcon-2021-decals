7ae7e6e785c452608af4e3ad3238675fff0a44d8
Hugo Locurcio <hugo.locurcio@hugo.pro>
2021-06-04 20:54:41 +0200

    Update materials and test scene to include blob shadows example

--- caustics.gd
+++ caustics.gd
@@ -4,8 +4,9 @@ var caustic_id := 0
 var timer := 0.0
 
 func _physics_process(some_timer):
-	timer += some_timer
-	if timer >= 0.04:  # 25 FPS
-		timer = 0.0
-		caustic_id = (caustic_id + 1) % 32
-		texture_emission = load("res://decals/caustics/caust%02d.png" % caustic_id)
+	if visible:
+		timer += some_timer
+		if timer >= 0.04:  # 25 FPS
+			timer = 0.0
+			caustic_id = (caustic_id + 1) % 32
+			texture_emission = load("res://decals/caustics/caust%02d.png" % caustic_id)
